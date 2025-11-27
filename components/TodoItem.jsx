afbcb0ece10417fb395c81a03ae3fa8db03cea56
TahaY291 <mtahayasin07@gmail.com>
2025-11-27 21:09:24 +0500

    setup done

---
+++ components/TodoItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TodoItem() {
+  return <div>TodoItems</div>;
+}
+
+export default TodoItem;
---
+++ firebase/firebase.config.js
@@ -0,0 +1,21 @@
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfDYPCT3TXCDtk5I_uFfx7ChEMz4ekU5k",
+  authDomain: "nextjs-todo-app-22040.firebaseapp.com",
+  projectId: "nextjs-todo-app-22040",
+  storageBucket: "nextjs-todo-app-22040.firebasestorage.app",
+  messagingSenderId: "999770713630",
+  appId: "1:999770713630:web:51bada176668ea732a880f",
+  measurementId: "G-879GJK0SR9"
+};
+
+export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
